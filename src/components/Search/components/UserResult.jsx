620882e5865b747bc8dd05c3244ee920eaaf1fb8
VinhHuuTran <tranhuuvinh1109@gmail.com>
2023-06-09 00:51:50 +0700

    change data and css

--- src/components/Search/components/UserResult.jsx
+++ src/components/Search/components/UserResult.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const UserResult = ({ user }) => {
+	console.log('user', user);
 	return (
 		<Link to={ `user/${user.user_id}` }>
 			<div className="flex mb-2 w-full items-center my-hover px-2 py-1 text-black">
@@ -13,7 +14,7 @@ const UserResult = ({ user }) => {
 				}
 				<div className="ml-2 flex flex-col justify-between">
 					{
-						user?.fullname && <h6 className="m-0 font-semibold">{ user.fullname }</h6>
+						user?.user_name && <h6 className="m-0 font-semibold">{ user.user_name }</h6>
 					}
 					{
 						user?.email && <h6 className="text-sm italic m-0 text-slate-600">{ user.email }</h6>
