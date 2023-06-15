b50c1a2ef8271fc7cc4510fb197ac9ce9cf14656
VinhHuuTran <tranhuuvinh1109@gmail.com>
2023-06-15 20:59:18 +0700

    change link id

--- src/components/Search/components/BlogResult.jsx
+++ src/components/Search/components/BlogResult.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 const BlogResult = ({ blog }) => {
 	return (
-		<Link to={ `blog/${blog.blog_id}` } >
+		<Link to={ `blog/${blog.id}` } >
 			<div className="flex mb-2 w-full items-center justify-between my-hover px-2 py-1 text-black">
 				{
 					blog?.image && <div className="w-4/12 mr-2"><img src={ blog.image } className="w-full max-h-20 object-cover rounded-lg" alt="blog" /></div>
